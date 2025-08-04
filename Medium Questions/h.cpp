35a256b29aed547f0c5623095d9677bea15ce71b
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-04 17:31:10 +0530

    h

---
+++ Medium Questions/h.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "This is a placeholder for the h.cpp file." << endl;
+    cout << "You can add your own code here." << endl;
+    return 0;
+}
