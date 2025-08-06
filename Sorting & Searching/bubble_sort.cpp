9796f232130bccc03de81e2b74e6d0c3e389b96b
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-06 11:41:50 +0530

    Create bubble_sort.cpp

---
+++ Sorting & Searching/bubble_sort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSortAscending(int arr[],int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j],arr[j + 1]);
+            }
+        }
+    }
+}
+
+void bubbleSortDecending(int arr[],int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] < arr[j + 1]) {
+                swap(arr[j],arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64,34,25,12,22,11,90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    bubbleSortAscending(arr, n);
+    cout << "Sorted array in Ascending Order: ";
+    printArray(arr, n);
+
+    bubbleSortDecending(arr, n);
+    cout << "Sorted array in Descending Order: ";
+    printArray(arr, n);
+    return 0;
+}
