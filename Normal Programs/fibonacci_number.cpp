3a8152439e2682547c9901ebdb64cebb19717996
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-06 12:16:03 +0530

    Create fibonacci_number.cpp

---
+++ Normal Programs/fibonacci_number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if(n == 0 || n == 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Enter the position of the Fibonacci number: ";
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Invalid input. Please enter a non-negative integer." << endl;
+        return 1;
+    }
+
+    int result = fibonacci(n);
+    cout << "Fibonacci number at position " << n << " is: " << result << endl;
+
+    return 0;
+}
