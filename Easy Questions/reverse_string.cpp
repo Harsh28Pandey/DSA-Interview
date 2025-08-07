4b44d2f127c6e9c7cd0958a131c85daaf8c49310
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-07 15:45:39 +0530

    Create reverse_string.cpp

---
+++ Easy Questions/reverse_string.cpp
@@ -0,0 +1,24 @@
+// Problem - Reverse String (344) - Leetcode
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void reverseString(vector<char>& s) {
+    int left = 0, right = s.size() - 1;
+    while (left < right) {
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+    reverseString(s);
+    for (char c : s) {
+        cout << c;
+    }
+    return 0;
+}
