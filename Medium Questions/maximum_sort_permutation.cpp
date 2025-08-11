c205c77c1ac81ea76632d85201b99d10d4409ec5
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-11 21:11:34 +0530

    Create maximum_sort_permutation.cpp

---
+++ Medium Questions/maximum_sort_permutation.cpp
@@ -0,0 +1,20 @@
+// Problem - Maximum K to Sort Permutations (3644) - LeetCode
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int sortPermutation(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> sorted = nums;
+    sort(sorted.begin(), sorted.end());
+    int ans = -1; // sentinel meaning "no mismatch seen yet"
+
+    for (int i = 0; i < n; ++i) {
+        if (nums[i] != sorted[i]) {
+            if (ans == -1) ans = nums[i];
+            else ans &= nums[i];
+        }
+    }
+    return ans == -1 ? 0 : ans;
+}
