07550ba45fe9b4664029ded1c58290924128a414
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-11 17:57:12 +0530

    Create Day 2-Problem No. 869.cpp

---
+++ LeetCode POTD/Day 2-Problem No. 869.cpp
@@ -0,0 +1,17 @@
+// Problem - Reordered Power of 2 (869) - LeetCode
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool reorderedPowerOf2(int n) {
+    string s = to_string(n);
+    sort(s.begin(),s.end());
+    for(int i = 0; i < 31; i++) {
+        string t = to_string(1 << i);
+        sort(t.begin(),t.end());
+        if(s == t) 
+            return true;
+    } 
+    return false;
+}
