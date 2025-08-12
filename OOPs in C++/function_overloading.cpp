f75e9561d799fbd5e4c9fa4864d0ec1003f243e3
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-12 11:28:04 +0530

    Create function_overloading.cpp

---
+++ OOPs in C++/function_overloading.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Area {
+    public:
+    int calculate_area(int r) {
+        return 3.14 * r * r;
+    }
+    int calculate_area(int l,int b) {
+        return l * b;
+    }
+};
+
+int main()
+{
+    Area A1,A2;
+    cout<<A1.calculate_area(4)<<endl;
+    cout<<A2.calculate_area(3,4)<<endl;
+}
