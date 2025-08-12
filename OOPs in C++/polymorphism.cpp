b38c99237899180565e51b9491c6e261f4b631a6
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-12 11:30:20 +0530

    Create polymorphism.cpp

---
+++ OOPs in C++/polymorphism.cpp
@@ -0,0 +1,9 @@
+// In polymorphism there are three concets are :-
+// 1 - Function Overloading
+// 2 - Operator Overloading
+// 3 - Virtual Function
+
+// then these concepts should be write into a seprate file name as 
+// function_overloading()
+// operator_overloading()
+// virtual_function()
