d45e4aaa7ab8cf66bf143e186f58e1860ba00cd9
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-14 16:13:20 +0530

    Create insert_at_start.cpp

---
+++ Double Linked List in C++/insert_at_start.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int value) {
+        data = value;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = NULL;
+
+    // insert at start
+    // linked list doesn't exist
+    if(head == NULL) {
+        head = new Node(5);
+    }
+
+    // linked list already exist
+    else {
+        Node *temp = new Node(5);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+
+    // print the values
+    Node *trav = head;
+    while(trav) {
+        cout << trav->data << " ";
+        trav = trav->next;
+    }
+}
