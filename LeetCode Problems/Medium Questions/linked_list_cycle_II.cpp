ee48d1dc800487d3ffe92a8addece08f96ee8bdc
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-15 17:01:52 +0530

    Create linked_list_cycle_II.cpp

---
+++ LeetCode Problems/Medium Questions/linked_list_cycle_II.cpp
@@ -0,0 +1,28 @@
+// Problem - Linked List Cycle II - (142) - LeetCode
+
+#include<iostream>
+using namespace std;
+
+ListNode *detectCycle(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+    bool isCycle = false;
+
+    while(fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) {
+            isCycle = true;
+            break;
+        }
+    }
+    if(!isCycle) {
+        return NULL;
+    }
+    slow = head;
+    while(slow != fast) {
+        slow = sloe->next;
+        fast = fast->next;
+    }
+    return slow;
+}
