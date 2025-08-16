f9051377f85b0b03bfbe245c01448973df34731f
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-16 16:13:32 +0530

    Create Day 8-Problem No. 1323.cpp

---
+++ LeetCode Problems/LeetCode POTD/Day 8-Problem No. 1323.cpp
@@ -0,0 +1,12 @@
+// Problem - Maximum 69 Number (1323) - LeetCode
+
+int maximum69Number (int num) {
+    string s = to_string(num);   // convert number to string
+    for (char &c : s) {
+        if (c == '6') {          // change the first '6' to '9'
+            c = '9';
+            break;               // only one change allowed
+        }
+    }
+    return stoi(s);              // convert back to integer
+}
