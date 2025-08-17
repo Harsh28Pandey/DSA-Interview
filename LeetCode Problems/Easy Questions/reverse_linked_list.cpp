9c06e077dfac96a25e3ff2b390e1b94e2b0379c1
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:45:57 +0530

    Update reverse_linked_list.cpp

--- LeetCode Problems/Easy Questions/reverse_linked_list.cpp
+++ LeetCode Problems/Easy Questions/reverse_linked_list.cpp
@@ -1,7 +1,14 @@
 // Problem - Reverse Linked List (206) - LeetCode
 
 #include<iostream>
+#include<vector>
 using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
 ListNode *reverseList(ListNode *head) {
     ListNode *prev = NULL;
@@ -14,4 +21,45 @@ ListNode *reverseList(ListNode *head) {
         curr = next;
     }
     return prev;
+}
+
+ListNode* createList(const vector<int>& vals) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    for (int v : vals) {
+        tail->next = new ListNode(v);
+        tail = tail->next;
+    }
+    return dummy.next;
+}
+
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val;
+        if (head->next != nullptr) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> vals = {1, 2, 3, 4, 5};
+    ListNode* head = createList(vals);
+
+    cout << "Original list: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed list: ";
+    printList(head);
+
+    // Cleanup memory
+    while (head != nullptr) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
 }
