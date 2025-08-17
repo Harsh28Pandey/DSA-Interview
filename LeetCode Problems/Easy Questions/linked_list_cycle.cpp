deec6672f4e8af834b0fdab65abb70a1a28eb29f
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:39:15 +0530

    Update linked_list_cycle.cpp

--- LeetCode Problems/Easy Questions/linked_list_cycle.cpp
+++ LeetCode Problems/Easy Questions/linked_list_cycle.cpp
@@ -3,6 +3,12 @@
 #include<iostream>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 bool hasCycle(ListNode *head) {
     ListNode *slow = head;
     ListNode *fast = head;
@@ -14,4 +20,31 @@ bool hasCycle(ListNode *head) {
         }
     }
     return false;
+}
+
+int main() {
+    // Create nodes
+    ListNode* node1 = new ListNode(3);
+    ListNode* node2 = new ListNode(2);
+    ListNode* node3 = new ListNode(0);
+    ListNode* node4 = new ListNode(-4);
+
+    // Connect nodes: 3 -> 2 -> 0 -> -4
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+
+    // Create cycle: -4 points back to node2
+    node4->next = node2;
+
+    if(hasCycle(node1)) {
+        cout << "Cycle detected in the linked list.\n";
+    } else {
+        cout << "No cycle in the linked list.\n";
+    }
+
+    // Important: Since there's a cycle, normal deletion would loop infinitely.
+    // For demo purpose, we skip deletion here.
+
+    return 0;
 }
