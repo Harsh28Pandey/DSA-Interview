a9e0d868713d7019294a396e60368505e641e63e
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:31:05 +0530

    Update xor_after_range_I.cpp

--- LeetCode Problems/Medium Questions/xor_after_range_I.cpp
+++ LeetCode Problems/Medium Questions/xor_after_range_I.cpp
@@ -25,3 +25,29 @@ int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
     }
     return result;
 }
+
+int main() {
+    int n, q;
+    cout << "Enter the size of nums array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter " << n << " elements of nums:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter the number of queries: ";
+    cin >> q;
+
+    vector<vector<int>> queries(q, vector<int>(4));
+    cout << "Enter queries in format: li ri ki vi (0-based indices for li and ri)\n";
+    for (int i = 0; i < q; i++) {
+        cin >> queries[i][0] >> queries[i][1] >> queries[i][2] >> queries[i][3];
+    }
+
+    int ans = xorAfterQueries(nums, queries);
+    cout << "Result after all queries (XOR of all elements): " << ans << endl;
+
+    return 0;
+}
