ca749a350f6efd8ffdb966fda74b43c8b01e6323
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 15:57:51 +0530

    Create time_needed_buy_tickets.cpp

---
+++ LeetCode Problems/Easy Questions/time_needed_buy_tickets.cpp
@@ -0,0 +1,27 @@
+// Problem - Time Needed to Buy Tickets (2073) - LeetCode
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+
+int timeRequiredToBuy(vector<int>& tickets, int k) {
+    queue<int>q;
+    int n = tickets.size();
+    for(int i = 0; i < n; i++) {
+        q.push(i);
+    }
+    int time = 0;
+    while(tickets[k] != 0) {
+        tickets[q.front()]--;
+        // kya line mein lagega
+        if(tickets[q.front()]) {
+            q.push(q.front());
+        }
+        q.pop();
+        time++;
+    }
+    return time;
+}
