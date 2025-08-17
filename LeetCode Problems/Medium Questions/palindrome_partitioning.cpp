f7aa7d2388105ac8e543123fe276150247eb9b36
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:22:45 +0530

    Update palindrome_partitioning.cpp

--- LeetCode Problems/Medium Questions/palindrome_partitioning.cpp
+++ LeetCode Problems/Medium Questions/palindrome_partitioning.cpp
@@ -1,47 +1,53 @@
 // Problem - Palindrome Partitioning (131) - LeetCode
 // Time Complexity - O(n * 2^n)
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-void getAllPartitions(string s, vector<string> &partitions, vector<vector<string>> &ans) {
-    if(s.size() == 0) {
+// Check if a string is a palindrome
+bool isPalindrome(string s) {
+    string s2 = s;
+    reverse(s2.begin(), s2.end());
+    return s == s2;
+}
+
+// Recursive helper function to get all palindrome partitions
+void getAllPartitions(string s, vector<string>& partitions, vector<vector<string>>& ans) {
+    if (s.size() == 0) {
         ans.push_back(partitions);
-        return ;
+        return;
     }
-    for(int i = 0; i < s.size(); i++) {
-        string part = s.substr(0,i + 1);
-        if(isPalindrome(part)) {
+    for (int i = 0; i < s.size(); i++) {
+        string part = s.substr(0, i + 1);
+        if (isPalindrome(part)) {
             partitions.push_back(part);
-            getAllPartitions(s.substr(i + 1),partitions,ans);
+            getAllPartitions(s.substr(i + 1), partitions, ans);
             partitions.pop_back();
         }
     }
 }
 
-bool isPalindrome(string s) {
-    string s2 = s;
-    reverse(s2.begin(),s2.end());
-    return s == s2;
-}
-
+// Main partitioning function
 vector<vector<string>> partition(string s) {
     vector<vector<string>> ans;
     vector<string> partitions;
-    getAllPartitions(s,partitions,ans);
+    getAllPartitions(s, partitions, ans);
     return ans;
 }
 
 int main() {
     string str = "aab";
     vector<vector<string>> ans = partition(str);
-    for(int i = 0; i < ans.size(); i++) {
-        for(int j = 0; j < ans[i].size(); j++) {
-            cout << ans[i][j] << " ";
+
+    cout << "Palindrome partitions of \"" << str << "\":" << endl;
+    for (const auto& vec : ans) {
+        for (const auto& part : vec) {
+            cout << part << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
