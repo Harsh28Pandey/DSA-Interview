b4992fa6042f6158d0270d51774c5800f7309887
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:19:08 +0530

    Update copy_list_random_pointer.cpp

--- LeetCode Problems/Medium Questions/copy_list_random_pointer.cpp
+++ LeetCode Problems/Medium Questions/copy_list_random_pointer.cpp
@@ -5,6 +5,19 @@
 #include<unordered_map>
 using namespace std;
 
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = nullptr;
+        random = nullptr;
+    }
+};
+
 Node *copyRandom(Node *head) {
     if(head == NULL) {
         return NULL;
@@ -30,4 +43,49 @@ Node *copyRandom(Node *head) {
         newTemp = newTemp->next;
     }
     return newHead;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp) {
+        cout << "Node value: " << temp->val;
+        if (temp->random)
+            cout << ", Random points to: " << temp->random->val;
+        else
+            cout << ", Random points to: NULL";
+        cout << endl;
+        temp = temp->next;
+    }
+}
+
+int main() {
+    // Create nodes
+    Node* node1 = new Node(7);
+    Node* node2 = new Node(13);
+    Node* node3 = new Node(11);
+    Node* node4 = new Node(10);
+    Node* node5 = new Node(1);
+
+    // Connect next pointers
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+
+    // Set random pointers
+    node2->random = node1;
+    node3->random = node5;
+    node4->random = node3;
+    node5->random = node1;
+
+    // Deep copy the list
+    Node* copiedHead = copyRandom(node1);
+
+    cout << "Original list:\n";
+    printList(node1);
+
+    cout << "\nCopied list:\n";
+    printList(copiedHead);
+
+    return 0;
 }
