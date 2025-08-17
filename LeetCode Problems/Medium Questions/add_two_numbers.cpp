06db4622fd904217514a111a06f1150813eafbb0
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:21:21 +0530

    Update add_two_numbers.cpp

--- LeetCode Problems/Medium Questions/add_two_numbers.cpp
+++ LeetCode Problems/Medium Questions/add_two_numbers.cpp
@@ -3,6 +3,12 @@
 #include<iostream>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     ListNode dummy(0); // Dummy head node for easier handling
     ListNode* current = &dummy;
@@ -28,4 +34,34 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     }
 
     return dummy.next; // skip dummy head
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Number 1: 342 (represented as 2 -> 4 -> 3)
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    // Number 2: 465 (represented as 5 -> 6 -> 4)
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    // Add the two numbers
+    ListNode* result = addTwoNumbers(l1, l2);
+
+    // Output the result
+    cout << "Result: ";
+    printList(result);
+
+    return 0;
 }
