85bd2a81ddeee3dd04ad93a73309c5ee24955e91
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:42:25 +0530

    Update merge_two_sorted_list.cpp

--- LeetCode Problems/Easy Questions/merge_two_sorted_list.cpp
+++ LeetCode Problems/Easy Questions/merge_two_sorted_list.cpp
@@ -1,14 +1,17 @@
 // Problem - Merge Two Sorted List (21) - LeetCode
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    // Create a dummy node to simplify edge cases
-    ListNode dummy;
+    ListNode dummy(0);
     ListNode* tail = &dummy;
 
-    // Compare and attach smaller nodes one by one
     while (list1 && list2) {
         if (list1->val <= list2->val) {
             tail->next = list1;
@@ -20,9 +23,56 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         tail = tail->next;
     }
 
-    // Attach remaining nodes (only one of these will execute)
     if (list1) tail->next = list1;
     if (list2) tail->next = list2;
 
-    return dummy.next; // Skip dummy and return actual head
+    return dummy.next;
+}
+
+// Helper function to print linked list
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+// Helper function to create linked list from vector
+ListNode* createList(const vector<int>& vals) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    for (int v : vals) {
+        tail->next = new ListNode(v);
+        tail = tail->next;
+    }
+    return dummy.next;
+}
+
+int main() {
+    vector<int> vals1 = {1, 3, 5, 7};
+    vector<int> vals2 = {2, 4, 6, 8};
+
+    ListNode* list1 = createList(vals1);
+    ListNode* list2 = createList(vals2);
+
+    cout << "List 1: ";
+    printList(list1);
+
+    cout << "List 2: ";
+    printList(list2);
+
+    ListNode* merged = mergeTwoLists(list1, list2);
+    cout << "Merged List: ";
+    printList(merged);
+
+    // Clean up memory (optional here, but good practice)
+    while (merged) {
+        ListNode* temp = merged;
+        merged = merged->next;
+        delete temp;
+    }
+
+    return 0;
 }
