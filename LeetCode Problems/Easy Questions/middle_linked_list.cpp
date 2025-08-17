fad7bd74dc409a54f93e17185b9fc4b24ba13df4
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-17 16:44:05 +0530

    Update middle_linked_list.cpp

--- LeetCode Problems/Easy Questions/middle_linked_list.cpp
+++ LeetCode Problems/Easy Questions/middle_linked_list.cpp
@@ -1,9 +1,16 @@
 // Problem - Middle of the Linked List (876) - LeetCode
 
 #include<iostream>
+#include<vector>
 using namespace std;
 
-ListNode middleNode(ListNode *head) {
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* middleNode(ListNode *head) {
     ListNode *slow = head;
     ListNode *fast = head;
     while(fast != NULL && fast->next != NULL) {
@@ -11,4 +18,50 @@ ListNode middleNode(ListNode *head) {
         fast = fast->next->next;
     }
     return slow;
+}
+
+ListNode* createList(const vector<int>& vals) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    for (int v : vals) {
+        tail->next = new ListNode(v);
+        tail = tail->next;
+    }
+    return dummy.next;
+}
+
+// Helper to print list from a given node
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val;
+        if (head->next != nullptr) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> vals = {1, 2, 3, 4, 5};
+    ListNode* head = createList(vals);
+
+    cout << "Original list: ";
+    printList(head);
+
+    ListNode* mid = middleNode(head);
+    if (mid != nullptr) {
+        cout << "Middle node value: " << mid->val << endl;
+        cout << "List from middle node: ";
+        printList(mid);
+    } else {
+        cout << "List is empty." << endl;
+    }
+
+    // Cleanup memory
+    while (head != nullptr) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
 }
