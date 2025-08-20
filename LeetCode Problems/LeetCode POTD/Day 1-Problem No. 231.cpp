3d69e93d14f08b911f0bfe9d9eeb737f47c13f72
Harsh Pandey <harsh28.knp@gmail.com>
2025-08-20 10:35:33 +0530

    deleted

--- LeetCode Problems/LeetCode POTD/Day 1-Problem No. 231.cpp
+++
@@ -1,8 +0,0 @@
-// Problem - Power of 2 (231) - LeetCode
-
-bool isPowerOfTwo(int n) {
-    if (n <= 0) {
-        return false;
-    }
-    return (n & (n - 1)) == 0;
-}
--- LeetCode Problems/LeetCode POTD/Day 5-Problem No. 326.cpp
+++
@@ -1,9 +0,0 @@
-// Problem - Power of Three (326) - leetCode
-
-bool isPowerOfThree(int n) {
-    if (n <= 0) return false; // Negative and zero are not powers of three
-    while (n % 3 == 0) {
-        n /= 3;
-    }
-    return n == 1;
-}
