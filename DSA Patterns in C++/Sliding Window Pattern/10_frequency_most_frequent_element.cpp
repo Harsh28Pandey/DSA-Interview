0412aa43d2c7dc3d4bca1d6ba381c61f5bc3b555
Harsh Pandey <harsh28.knp@gmail.com>
2025-12-14 17:38:34 +0530

    sliding window pattern

--- DSA Patterns in C++/Sliding Window Pattern/10_frequency_most_frequent_element.cpp
+++ DSA Patterns in C++/Sliding Window Pattern/10_frequency_most_frequent_element.cpp
@@ -1,4 +1,27 @@
 // Problem - Frequency of the Most Frequent Element (1838) - LeetCode
-// Time Complexity - 
-// Space Complexity - 
+// Time Complexity - O(n logn)
+// Space Complexity - O(n)
 
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        int maximum = 0;
+        long sum = 0;
+        sort(nums.begin(),nums.end());
+        int i = 0;
+        int j = 0;
+
+        while(j < nums.size()) {
+            sum += nums[j];
+
+            while((long)nums[j] * (j - i + 1) - sum > k) {
+                sum -= nums[i];
+                i++;
+            }
+
+            maximum = max(maximum,j - i + 1);
+            j++;
+        }
+        return maximum;
+    }
+};
--- DSA Patterns in C++/Sliding Window Pattern/9_number_of_subarray_threshold.cpp
+++ DSA Patterns in C++/Sliding Window Pattern/9_number_of_subarray_threshold.cpp
@@ -1,4 +1,30 @@
 // Problem - Number of Subarrays of Size K and Average Greater than or Equal to Threshold (1343) - LeetCode
-// Time Complexity - 
-// Space Complexity - 
+// Time Complexity - O(n)
+// Space Complexity - O(1)
 
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int sum = 0;
+        int maxThreshold = k * threshold;
+
+        for(int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        int count = 0;
+        if(sum >= maxThreshold) {
+            count++;
+        }
+
+        for(int i = k; i < arr.size(); i++) {
+            sum += arr[i];
+            sum -= arr[i - k];
+
+            if(sum >= maxThreshold) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
